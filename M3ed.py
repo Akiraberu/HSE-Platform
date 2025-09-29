48eda05497ebc0039667055a74ea885d8a8b08f3
Akiraberu <regularmundanemann@gmail.com>
2025-09-29 23:59:44 +0100

    modified:   M3ed.py

---
+++ M3ed.py
@@ -0,0 +1,11 @@
+cached_fibonacci = dict()
+
+def fibonacci(n):
+    if n==0: return 0
+    if n==1: return 1
+    if str(n) in cached_fibonacci: return cached_fibonacci[str(n)]
+    answer = fibonacci(n-1) + fibonacci(n-2)
+    cached_fibonacci[str(n)] = answer
+    return answer
+
+print( fibonacci(10) )
